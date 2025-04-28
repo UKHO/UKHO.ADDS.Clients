f69477bae3a0612ed112fed28579db6143219ea3
Vipin Mahajan <vipin14904@mastek.com>
2025-04-28 18:50:10 +0530

    AB#214780 AB#218012 AB#218013

--- src/UKHO.ADDS.Clients.FileShareService.ReadWrite/FileShareReadWriteClient.cs
+++ src/UKHO.ADDS.Clients.FileShareService.ReadWrite/FileShareReadWriteClient.cs
@@ -1,4 +1,7 @@
-﻿using UKHO.ADDS.Clients.Common.Authentication;
+﻿using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using UKHO.ADDS.Clients.Common.Authentication;
 using UKHO.ADDS.Clients.FileShareService.ReadOnly;
 using UKHO.ADDS.Clients.FileShareService.ReadOnly.Models;
 using UKHO.ADDS.Clients.FileShareService.ReadWrite.Models;
@@ -22,9 +25,15 @@ namespace UKHO.ADDS.Clients.FileShareService.ReadWrite
 
         public Task<IResult<AppendAclResponse>> AppendAclAsync(string batchId, Acl acl, CancellationToken cancellationToken = default) => Task.FromResult<IResult<AppendAclResponse>>(Result.Success(new AppendAclResponse()));
 
-        public Task<IResult<IBatchHandle>> CreateBatchAsync(BatchModel batchModel) => Task.FromResult<IResult<IBatchHandle>>(Result.Success<IBatchHandle>(new BatchHandle("batchid")));
+        public async Task<IResult<IBatchHandle>> CreateBatchAsync(BatchModel batchModel)
+        {
+            return await CreateBatchInternalAsync(batchModel, CancellationToken.None);
+        }
 
-        public Task<IResult<IBatchHandle>> CreateBatchAsync(BatchModel batchModel, CancellationToken cancellationToken) => Task.FromResult<IResult<IBatchHandle>>(Result.Success<IBatchHandle>(new BatchHandle("batchid")));
+        public async Task<IResult<IBatchHandle>> CreateBatchAsync(BatchModel batchModel, CancellationToken cancellationToken)
+        {
+            return await CreateBatchInternalAsync(batchModel, cancellationToken);
+        }
 
         public Task<IResult<BatchStatusResponse>> GetBatchStatusAsync(IBatchHandle batchHandle) => Task.FromResult<IResult<BatchStatusResponse>>(Result.Success(new BatchStatusResponse()));
 
@@ -48,5 +57,33 @@ namespace UKHO.ADDS.Clients.FileShareService.ReadWrite
         public Task<IResult<RollBackBatchResponse>> RollBackBatchAsync(IBatchHandle batchHandle, CancellationToken cancellationToken) => Task.FromResult<IResult<RollBackBatchResponse>>(Result.Success(new RollBackBatchResponse()));
 
         public Task<IResult<SetExpiryDateResponse>> SetExpiryDateAsync(string batchId, BatchExpiryModel batchExpiry, CancellationToken cancellationToken = default) => Task.FromResult<IResult<SetExpiryDateResponse>>(Result.Success(new SetExpiryDateResponse()));
+
+        private async Task<IResult<IBatchHandle>> CreateBatchInternalAsync(BatchModel batchModel, CancellationToken cancellationToken)
+        {
+            var uri = new Uri($"batch", UriKind.Relative);
+
+            try
+            {
+                using var httpClient = await GetAuthenticationHeaderSetClient();
+                using var httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, uri)
+                {
+                    Content = new StringContent(JsonSerializer.Serialize(batchModel), Encoding.UTF8, "application/json")
+                };
+
+                var response = await httpClient.SendAsync(httpRequestMessage, cancellationToken);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    return Result.Failure<IBatchHandle>($"Failed to create batch. Status code: {response.StatusCode}");
+                }
+
+                var batchHandle = await response.Content.ReadFromJsonAsync<BatchHandle>(cancellationToken: cancellationToken);
+                return Result.Success<IBatchHandle>(batchHandle);
+            }
+            catch (Exception ex)
+            {
+                return Result.Failure<IBatchHandle>(ex.Message);
+            }
+        }
     }
 }
---
+++ test/UKHO.ADDS.Clients.FileShareService.ReadWrite.Tests/FileShareReadWriteClientTest.cs
@@ -0,0 +1,88 @@
+﻿using System.Net;
+using System.Text;
+using System.Text.Json;
+using FakeItEasy;
+using NUnit.Framework;
+using UKHO.ADDS.Clients.Common.Authentication;
+using UKHO.ADDS.Clients.FileShareService.ReadWrite.Models;
+
+namespace UKHO.ADDS.Clients.FileShareService.ReadWrite.Tests
+{
+    [TestFixture]
+    internal class FileShareReadWriteClientTest
+    {
+        private IHttpClientFactory _httpClientFactory;
+        private IAuthenticationTokenProvider _authTokenProvider;
+        private FileShareReadWriteClient _client;
+        private BatchModel _batchModel;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _httpClientFactory = A.Fake<IHttpClientFactory>();
+            _authTokenProvider = A.Fake<IAuthenticationTokenProvider>();
+            _client = new FileShareReadWriteClient(_httpClientFactory, "http://test.com", _authTokenProvider);
+        }
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _batchModel = new BatchModel
+            {
+                BusinessUnit = "TestUnit",
+                Acl = new Acl { ReadUsers = new[] { "user1" } },
+                Attributes = new List<KeyValuePair<string, string>> { new("key", "value") },
+                ExpiryDate = DateTime.UtcNow.AddDays(1)
+            };
+        }
+
+        [Test]
+        public async Task WhenCreateBatchAsyncWithValidBatchModel_ThenReturnsSuccessResult()
+        {
+            var httpClient = A.Fake<HttpClient>();
+            var responseMessage = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(JsonSerializer.Serialize(new BatchHandle("batchId")), Encoding.UTF8, "application/json")
+            };
+
+            A.CallTo(() => _httpClientFactory.CreateClient(A<string>._)).Returns(httpClient);
+            A.CallTo(() => httpClient.SendAsync(A<HttpRequestMessage>._, A<CancellationToken>._)).Returns(Task.FromResult(responseMessage));
+
+            var result = await _client.CreateBatchAsync(_batchModel);
+
+            result.IsSuccess(out var value, out var error);
+
+            Assert.That(result.IsSuccess, Is.True);
+            Assert.That(value?.BatchId, Is.EqualTo("batchId"));
+        }
+
+        [Test]
+        public async Task WhenCreateBatchAsyncWithInvalidResponse_ThenReturnsFailureResult()
+        {
+            var httpClient = A.Fake<HttpClient>();
+            var responseMessage = new HttpResponseMessage(HttpStatusCode.BadRequest);
+
+            A.CallTo(() => _httpClientFactory.CreateClient(A<string>._)).Returns(httpClient);
+            A.CallTo(() => httpClient.SendAsync(A<HttpRequestMessage>._, A<CancellationToken>._)).Returns(Task.FromResult(responseMessage));
+
+            var result = await _client.CreateBatchAsync(_batchModel);
+
+            Assert.That(result.IsSuccess, Is.False);
+            Assert.That(result.Errors.FirstOrDefault()?.Message, Is.EqualTo("Failed to create batch. Status code: BadRequest"));
+        }
+
+        [Test]
+        public async Task WhenCreateBatchAsyncWithException_ThenReturnsFailureResult()
+        {
+            var httpClient = A.Fake<HttpClient>();
+
+            A.CallTo(() => _httpClientFactory.CreateClient(A<string>._)).Returns(httpClient);
+            A.CallTo(() => httpClient.SendAsync(A<HttpRequestMessage>._, A<CancellationToken>._)).Throws(new Exception("Test exception"));
+
+            var result = await _client.CreateBatchAsync(_batchModel);
+
+            Assert.That(result.IsSuccess, Is.False);
+            Assert.That(result.Errors.FirstOrDefault()?.Message, Is.EqualTo("Test exception"));
+        }
+    }
+}

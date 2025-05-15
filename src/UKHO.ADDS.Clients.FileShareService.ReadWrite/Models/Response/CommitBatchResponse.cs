78d56a8397b1b22ef703449346e7d977eba6f7a3
Mohammed Khan <mohamme15315@mastek.com>
2025-05-15 09:34:24 +0530

    Added JsonPropertyName attribute AB#219890

--- src/UKHO.ADDS.Clients.FileShareService.ReadWrite/Models/Response/CommitBatchResponse.cs
+++ src/UKHO.ADDS.Clients.FileShareService.ReadWrite/Models/Response/CommitBatchResponse.cs
@@ -1,12 +1,16 @@
-﻿namespace UKHO.ADDS.Clients.FileShareService.ReadWrite.Models.Response
+﻿using System.Text.Json.Serialization;
+
+namespace UKHO.ADDS.Clients.FileShareService.ReadWrite.Models.Response
 {
     public class CommitBatchResponse
     {
+        [JsonPropertyName("status")]
         public CommitBatchStatus Status { get; set; }
     }
 
     public class CommitBatchStatus
     {
+        [JsonPropertyName("uri")]
         public string Uri { get; set; }
     }
 }

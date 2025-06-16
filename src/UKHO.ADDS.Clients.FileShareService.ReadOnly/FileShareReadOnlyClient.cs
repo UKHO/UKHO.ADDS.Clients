2907401ad14f3e14458fdcb966f5dc638b99fded
Pradnya Bhosale <pradn1501261@mastek.com>
2025-06-16 17:30:46 +0530

    added change to handle batch search error response

--- src/UKHO.ADDS.Clients.FileShareService.ReadOnly/FileShareReadOnlyClient.cs
+++ src/UKHO.ADDS.Clients.FileShareService.ReadOnly/FileShareReadOnlyClient.cs
@@ -76,9 +76,8 @@ namespace UKHO.ADDS.Clients.FileShareService.ReadOnly
         public async Task<IResult<BatchSearchResponse>> SearchAsync(string searchQuery, int? pageSize, int? start, string correlationId, CancellationToken cancellationToken)
         {
             var response = await SearchResponse(searchQuery, pageSize, start, correlationId, cancellationToken);
-            var errorMetadata = await response.CreateErrorMetadata(ApiNames.FileShareService, correlationId);
 
-            return await response.CreateResultAsync<BatchSearchResponse, ErrorResponseModel>((errorResponseModel, status) => TranslateErrors(errorResponseModel, status, errorMetadata));
+            return await response.CreateResultAsync<BatchSearchResponse>(ApiNames.FileShareService, correlationId);            
         }
 
         protected IError TranslateErrors(ErrorResponseModel errorResponseModel, HttpStatusCode status, IDictionary<string, object> errorMetadata)

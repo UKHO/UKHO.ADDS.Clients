702dff1f1e9950fbc73d8cab2b6e9255df92bf31
DanOwenUKHO <118909036+DanOwenUKHO@users.noreply.github.com>
2025-06-17 18:20:20 +0100

    Refactor Kiota scripts to use relative paths and remove Kiota installation checks

--- deployment/RunKiota.ps1
+++ deployment/RunKiota.ps1
@@ -8,22 +8,6 @@ param (
 
 $env:PATH += ";$env:USERPROFILE\.dotnet\tools"
 
-# Ensure Kiota is installed
-$packageName = "Microsoft.Kiota.Bundle"
-Write-Host "Checking if Kiota is installed on $OutputDirectory.csproj..."
-$csprojPath = Get-ChildItem -Path . -Recurse -Filter "*$OutputDirectory.csproj" | Select-Object -First 1
-if ($null -eq $csprojPath) {
-    Write-Error "Could not find $OutputDirectory.csproj"
-    exit 1
-}
-[xml]$csproj = Get-Content $csprojPath.FullName
-
-$packageExist = $csproj.Project.ItemGroup.PackageReference | Where-Object { $_.Include -eq $packageName }
-
-if (-Not $packageExist) {
-    dotnet add $csprojPath.FullName package $packageName
-}
-
 $cmd = @(
     "kiota generate",
     "--openapi $OpenApiSpecPath",

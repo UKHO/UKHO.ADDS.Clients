07047658c93684443700ff5971178b3c1dbecd75
DanOwenUKHO <118909036+DanOwenUKHO@users.noreply.github.com>
2025-07-16 16:08:30 +0100

    Update helpers for easier registering clients

--- src/UKHO.ADDS.Clients.Common/MiddlewareExtensions/KiotaMiddlewareExtensions.cs
+++ src/UKHO.ADDS.Clients.Common/MiddlewareExtensions/KiotaMiddlewareExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Kiota.Abstractions.Authentication;
 using Microsoft.Kiota.Http.HttpClientLibrary;
 
 namespace UKHO.ADDS.Clients.Common.MiddlewareExtensions
@@ -9,6 +10,13 @@ namespace UKHO.ADDS.Clients.Common.MiddlewareExtensions
     /// </summary>
     public static class KiotaServiceCollectionExtensions
     {
+        public static void AddKiotaDefaults<T>(this IServiceCollection services, T authProvider) where T : IAuthenticationProvider
+        {
+            services.AddKiotaHandlers();
+            services.AddSingleton<ClientFactory>();
+            services.AddSingleton<IAuthenticationProvider>(authProvider);
+        }
+
         /// <summary>
         /// Adds the Kiota handlers to the service collection.
         /// </summary>
@@ -34,7 +42,7 @@ namespace UKHO.ADDS.Clients.Common.MiddlewareExtensions
         /// <param name="builder"></param>
         /// <param name="kiotaClientFactory">Factory to get handler types</param>
         /// <returns></returns>
-        public static IHttpClientBuilder AttachKiotaHandlers(this IHttpClientBuilder builder)
+        private static IHttpClientBuilder AttachKiotaHandlers(this IHttpClientBuilder builder)
         {
             // Dynamically load the Kiota handlers from the Client Factory
             var kiotaHandlers = KiotaClientFactory.GetDefaultHandlerActivatableTypes();
@@ -54,15 +62,31 @@ namespace UKHO.ADDS.Clients.Common.MiddlewareExtensions
         /// <param name="services">Service Collection for the target application</param>
         /// <param name="endpointConfigKey">EndPoint configuration value</param>
         /// <returns></returns>
-        public static IHttpClientBuilder AddConfiguredHttpClient<TClient>(this IServiceCollection services, string endpointConfigKey)
-                                                                            where TClient : class
+        public static IHttpClientBuilder AddConfiguredHttpClient<TClient>(this IServiceCollection services, string endpointConfigKey,
+                                                                          IDictionary<string, string>? headers = null)
+            where TClient : class
         {
             return services.AddHttpClient<TClient>((provider, client) =>
             {
                 var config = provider.GetRequiredService<IConfiguration>();
                 var endpoint = config[endpointConfigKey]!;
                 client.BaseAddress = new Uri(endpoint);
+
+                if (headers != null)
+                {
+                    foreach (var header in headers)
+                    {
+                        client.DefaultRequestHeaders.Add(header.Key, header.Value);
+                    }
+                }
             }).AttachKiotaHandlers();
+        }
+
+        public static void RegisterKiotaClient<TClient>(this IServiceCollection services, string endpointConfigKey, IDictionary<string, string>? headers = null)
+            where TClient : class
+        {
+            services.AddConfiguredHttpClient<TClient>(endpointConfigKey, headers);
+            services.AddSingleton(sp => sp.GetRequiredService<ClientFactory>().GetClient<TClient>());
         }
     }
 }

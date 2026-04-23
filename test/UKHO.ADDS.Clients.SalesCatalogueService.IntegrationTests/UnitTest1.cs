420673bdc56c0350b8ff8b75d8c5fd623391aefa
hukho <128484816+hukho@users.noreply.github.com>
2026-04-23 13:32:06 +0100

    Add SCS test project

---
+++ test/UKHO.ADDS.Clients.SalesCatalogueService.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace UKHO.ADDS.Clients.SalesCatalogueService.IntegrationTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
